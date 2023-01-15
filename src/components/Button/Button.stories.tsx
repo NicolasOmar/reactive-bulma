110a5319afbdb15ee005b618c3a66a9ac9dc0d7b
NicolasOmar <nicolas.passerino@gmail.com>
2023-01-14 21:14:41 -0300

    Storybook configured for React Typescript configuration. Need to solve compatibility with rollup

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const BasicExample = Template.bind({});
+BasicExample.args = {
+  text: 'Hello there'
+}
