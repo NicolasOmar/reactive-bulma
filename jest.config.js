4d29be36d03d330dde5748a98143423c1ffe1511
NicolasOmar <nicolas.passerino@gmail.com>
2023-02-06 00:39:11 -0300

    test: Adding coverageThreshold for check minimun test coverage of 80%

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.tsx',
     '!src/**/*.stories.tsx'
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  }
 };
