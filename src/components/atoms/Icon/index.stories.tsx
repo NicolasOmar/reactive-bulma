791719183680e5b4d7838ce40008dc52b8452c16
NicolasOmar <nicolas.passerino@gmail.com>
2023-05-10 21:04:04 -0300

    Adding Stories and more information on its controls

--- src/components/atoms/Icon/index.stories.tsx
+++ src/components/atoms/Icon/index.stories.tsx
@@ -4,11 +4,12 @@ import { StoryFn, Meta } from '@storybook/react'
 import Icon from '.'
 // MOCKS
 import mocks from './index.mocks.json'
+import { IconProps } from '../../../interfaces/atomProps'
 
 export default {
   title: 'Atoms/Icon',
   component: Icon,
-  // argTypes: mocks.storybook,
+  argTypes: mocks.storybook,
   args: mocks.testing.baseExample
 } as Meta<typeof Icon>
 
@@ -16,3 +17,27 @@ const Template: StoryFn<typeof Icon> = args => <Icon {...args} />
 
 export const BasicExample = Template.bind({})
 BasicExample.storyName = 'Basic Example'
+
+export const TextIncluded = Template.bind({})
+TextIncluded.storyName = 'With text'
+TextIncluded.args = mocks.testing.textIncluded as IconProps
+
+export const UsingOtherIcon = Template.bind({})
+UsingOtherIcon.storyName = 'With other icon'
+UsingOtherIcon.args = mocks.testing.otherIcon as IconProps
+
+export const Colored = Template.bind({})
+Colored.storyName = 'Colored'
+Colored.args = mocks.testing.colored as IconProps
+
+export const BigSize = Template.bind({})
+BigSize.storyName = 'Big Size'
+BigSize.args = mocks.testing.bigSize as IconProps
+
+export const WithDarkMode = Template.bind({})
+WithDarkMode.storyName = 'With Dark Mode'
+WithDarkMode.args = mocks.testing.withDarkMode as IconProps
+
+export const Spinning = Template.bind({})
+Spinning.storyName = 'Spinning'
+Spinning.args = mocks.testing.withSpin as IconProps
--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { IconProps } from '../../../interfaces/atomProps'
 import { parseClasses } from '../../../functions/parsers'
 import { textColorType } from '../../../types/styleTypes'
+import { parseTestId } from '../../../functions/parsers'
 
 const iconSizes = {
   'is-small': 24,
@@ -32,9 +33,11 @@ const Icon: React.FC<IconProps> = ({
     isSpinning ? 'mdi-spin' : null,
     size ? `mdi-${iconSizes[size]}px` : 'mdi-24px'
   ])
+  const _testId =
+    testId ?? parseTestId({ tag: 'icon', parsedClasses: iconClasses })
   const iconComponent = (
     <span
-      data-testid={testId}
+      data-testid={_testId}
       style={style || undefined}
       className={iconSpanClasses}
     >
