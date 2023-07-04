ecf37cc5a197a67ccf1766827c7dc737c2be4037
NicolasOmar <nicolas.passerino@gmail.com>
2023-07-04 17:20:23 -0300

    Adding stories and fixing small details on UT

--- src/components/atoms/File/index.test.tsx
+++ src/components/atoms/File/index.test.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
 // COMPONENTS
 import File from '.'
 // TYPES & INTERFACES
 // MOCKS
+import mocks from './index.mocks.json'
 
 describe('File', () => {
+  const { basicTestId, basicTestInputId, testClasses } = mocks.testing
+
   test('Should render without any props', () => {
     render(<File />)
-    const testFile = screen.getByTestId('test-file')
-    const testFileInput = screen.getByTestId('test-file-input')
+    const testFile = screen.getByTestId(basicTestId)
+    const testFileInput = screen.getByTestId(basicTestInputId)
     expect(testFile).toBeInTheDocument()
     expect(testFileInput).toBeInTheDocument()
+  })
+
+  test('Should render the Button with specfic classes', () => {
+    Object.keys(testClasses).forEach(prop => {
+      const classValue = (testClasses as Record<string, string>)[prop]
+      const classObj = { [prop]: classValue }
+      const testIdWithClass = `${basicTestId}-${classValue.replace('is-', '')}`
+      render(<File {...classObj} />)
+      const testClassButton = screen.getByTestId(testIdWithClass)
+      expect(testClassButton.className).toContain(classValue)
+      cleanup()
+    })
   })
 })
--- src/components/atoms/File/index.tsx
+++ src/components/atoms/File/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+// COMPONENTS
+import Icon from '../Icon'
 // TYPES & INTERFACES
 import { FileProps } from '../../../interfaces/atomProps'
-import { parseClasses, parseTestId } from '../../../functions/parsers'
-import Icon from '../Icon'
 // PARSERS
+import { parseClasses, parseTestId } from '../../../functions/parsers'
 
 const File: React.FC<FileProps> = ({
   testId = null,
   fileName = null,
-  uploadIcon = null,
+  uploadIcon = { iconLabel: 'upload' },
   uploadText = 'Choose a file…',
   style = null,
   buttonOnRight = false,
@@ -28,7 +29,21 @@ const File: React.FC<FileProps> = ({
     size
   ])
   const fileTestId =
-    testId ?? parseTestId({ tag: 'file', parsedClasses: fileClasses })
+    testId ??
+    parseTestId({
+      tag: 'file',
+      parsedClasses: fileClasses,
+      rules: [
+        {
+          usedRegExp: /has/gm,
+          regExpReplacer: '-has'
+        },
+        {
+          usedRegExp: /is|file/gm,
+          regExpReplacer: ''
+        }
+      ]
+    })
   const fileInputTestId = `${fileTestId}-input`
 
   return (
