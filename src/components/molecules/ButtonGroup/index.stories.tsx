3c764e139511ae7c3c6260eb922f353ab51e79b3
NicolasOmar <nicolas.passerino@gmail.com>
2023-09-19 10:04:50 -0300

    Adding story cases. Updating storybook to las version

--- src/components/molecules/ButtonGroup/index.stories.tsx
+++ src/components/molecules/ButtonGroup/index.stories.tsx
@@ -3,12 +3,11 @@ import { StoryFn, Meta } from '@storybook/react'
 // COMPONENTS
 import ButtonGroup from '.'
 // MOCKS
-import { testing, storybook } from './index.mocks.json'
+import { testing } from './index.mocks.json'
 
 export default {
   title: 'Molecules/ButtonGroup',
   component: ButtonGroup,
-  argTypes: storybook,
   args: testing.basicGroup
 } as Meta<typeof ButtonGroup>
 
@@ -16,3 +15,15 @@ const Template: StoryFn<typeof ButtonGroup> = args => <ButtonGroup {...args} />
 
 export const BasicGroup = Template.bind({})
 BasicGroup.storyName = 'Basic Group'
+
+export const AttachedButtons = Template.bind({})
+AttachedButtons.storyName = 'Attached Buttons'
+AttachedButtons.args = {
+  isAttached: true
+}
+
+export const RightPositioned = Template.bind({})
+RightPositioned.storyName = 'Positioned'
+RightPositioned.args = {
+  position: 'right'
+}
