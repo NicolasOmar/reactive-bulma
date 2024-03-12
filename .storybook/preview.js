b76a0b394d213c504e9f15a193cfb8c76b671782
NicolasOmar <nicolas.passerino@gmail.com>
2024-03-12 11:51:30 -0300

    Updating dependencies versions

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,6 @@ import '../node_modules/bulma/css/bulma.min.css'
 import '../node_modules/@mdi/font/css/materialdesignicons.min.css'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
