bc4bd400d70f4d3843f9c4f391756f053c6a9528
NicolasOmar <nicolas.passerino@gmail.com>
2024-05-21 23:39:08 -0300

    Updating dependencies versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,22 @@
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx', '../src/design/**/*.mdx'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-mdx-gfm',
-    '@storybook/addon-webpack5-compiler-babel',
-    '@chromatic-com/storybook'
+    '@storybook/addon-webpack5-compiler-babel'
   ],
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
   },
-  docs: {
-    autodocs: true
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
   }
 };
