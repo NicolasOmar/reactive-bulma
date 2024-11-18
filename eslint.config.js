f15b44c1fce4128934968d513d8e8d56c42a692e
NicolasOmar <nicolas.passerino@gmail.com>
2024-11-18 12:12:23 -0300

    Changing eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,30 @@
-// eslint.config.js
-import { fixupConfigRules } from "@eslint/compat"
-import { FlatCompat } from "@eslint/eslintrc"
-import js from "@eslint/js"
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-const flatCompat = new FlatCompat({
-  recommendedConfig: js.configs.recommended
-});
-
-export default [
-  ...fixupConfigRules(
-    flatCompat.extends(".eslintrc")
-  )
-];
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react': react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
