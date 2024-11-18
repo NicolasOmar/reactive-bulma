e1b245a15532a74d497ba2c8f3b672b89f9ae4a2
NicolasOmar <nicolas.passerino@gmail.com>
2024-11-18 11:31:34 -0300

    Changing storybook configurations based on new its 8.4.4 structure

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import '../node_modules/bulma/css/bulma.min.css'
+import '../node_modules/@mdi/font/css/materialdesignicons.min.css'
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
+      },
+      expanded: true
+    }
+  },
+  tags: ['autodocs']
+}
+
+export default preview
