c2031ad5012f683764027a131cdb8c441b74ef14
NicolasOmar <nicolas.passerino@gmail.com>
2025-09-22 18:01:29 +0200

    implementing storybook addon-themes addon with its custom decorator

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,18 +2,16 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.stories.tsx', '../src/design/**/*.mdx'],
-
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
-    '@storybook/addon-docs'
+    '@storybook/addon-docs',
+    '@storybook/addon-themes'
   ],
-
   framework: {
     name: '@storybook/react-vite',
     options: {}
   },
-
   typescript: {
     reactDocgen: 'react-docgen-typescript'
   }
