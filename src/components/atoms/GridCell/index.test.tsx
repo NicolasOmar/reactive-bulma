ce058b1a723d4801c7d4cb0c026b5e68e5db7be1
NicolasOmar <nicolas.passerino@gmail.com>
2025-11-18 13:35:11 +0100

    First implementation of Grid and GridCell components with stories and unit tests

---
+++ src/components/atoms/GridCell/index.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { cleanup, render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+// COMPONENTS
+import GridCell from '.'
+// TYPES & INTERFACES
+import { GridCellProps } from '@interfaces/atomProps'
+// FUNCTIONS
+// MOCKS
+import { testing } from './index.mocks.json'
+
+describe('GridCell', () => {
+  const { basicTestId, testClasses } = testing
+
+  test('Should render the component', () => {
+    render(<GridCell />)
+    const testGridCell = screen.getByTestId(basicTestId)
+
+    expect(testGridCell).toBeInTheDocument()
+  })
+
+  test.skip('Should render the component with specific classes', () => {
+    testClasses.forEach(({ name, value, result }) => {
+      const testIdWithClass = `${basicTestId}-${result.replace(
+        /is-|has-/gm,
+        ''
+      )}`
+      const classTestObject: GridCellProps = {
+        [name]: value
+      }
+
+      render(<GridCell {...classTestObject} />)
+
+      const testStylingPropValueGridCellGroup =
+        screen.getByTestId(testIdWithClass)
+      expect(testStylingPropValueGridCellGroup.className).toContain(result)
+      cleanup()
+    })
+  })
+})
