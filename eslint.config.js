48eff12a009e433846cfc87ebe1eb1b1fcc5d012
NicolasOmar <nicolas.passerino@gmail.com>
2026-05-18 11:20:56 +0200

    chore: updating conflictive dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -15,7 +14,6 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
